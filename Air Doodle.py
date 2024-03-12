a51ff633159d19f6e134b69ac8fbe6670824b0b4
kushagra gupta <kushagragupta99@gmail.com>
2024-03-12 21:07:26 +0530

    final

--- Air Doodle.py
+++ Air Doodle.py
@@ -86,8 +86,9 @@ while ret:
                 lmy = int(lm.y * 480)
 
                 landmarks.append([lmx, lmy])
-                # Drawing landmarks on frames
 
+
+        # Drawing landmarks on frames
         mpdraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
         fore_finger = (landmarks[8][0], landmarks[8][1])
         center = fore_finger
@@ -103,6 +104,62 @@ while ret:
             red_index += 1
             ypoints.append(deque(maxlen=512))
             yellow_index += 1
+        elif center[1] <= 65:
+            if 40 <= center[0] <= 140:  # Clear Button
+                bpoints = [deque(maxlen=512)]
+                gpoints = [deque(maxlen=512)]
+                rpoints = [deque(maxlen=512)]
+                ypoints = [deque(maxlen=512)]
+
+                blue_index = 0
+                green_index = 0
+                red_index = 0
+                yellow_index = 0
+
+                paintWindow[67:, :, :] = 255
+            elif 160 <= center[0] <= 255:
+                colorIndex = 0  # Blue
+            elif 275 <= center[0] <= 370:
+                colorIndex = 1  # Green
+            elif 390 <= center[0] <= 485:
+                colorIndex = 2  # Red
+            elif 505 <= center[0] <= 600:
+                colorIndex = 3  # Yellow
+        else:
+            if colorIndex == 0:
+                bpoints[blue_index].appendleft(center)
+            elif colorIndex == 1:
+                gpoints[green_index].appendleft(center)
+            elif colorIndex == 2:
+                rpoints[red_index].appendleft(center)
+            elif colorIndex == 3:
+                ypoints[yellow_index].appendleft(center)
+        # Append the next deques when nothing is detected to avois messing up
+    else:
+        bpoints.append(deque(maxlen=512))
+        blue_index += 1
+        gpoints.append(deque(maxlen=512))
+        green_index += 1
+        rpoints.append(deque(maxlen=512))
+        red_index += 1
+        ypoints.append(deque(maxlen=512))
+        yellow_index += 1
+
+ # Draw lines of all the colors on the canvas and frame
+    points = [bpoints, gpoints, rpoints, ypoints]
+    # for j in range(len(points[0])):
+    #         for k in range(1, len(points[0][j])):
+    #             if points[0][j][k - 1] is None or points[0][j][k] is None:
+    #                 continue
+    #             cv2.line(paintWindow, points[0][j][k - 1], points[0][j][k], colors[0], 2)
+    for i in range(len(points)):
+        for j in range(len(points[i])):
+            for k in range(1, len(points[i][j])):
+                if points[i][j][k - 1] is None or points[i][j][k] is None:
+                    continue
+                cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+                cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+
 
 
     cv2.imshow("Output", frame)
--- README.md
+++ README.md
@@ -1,19 +1,50 @@
 # Air Doodle
-A project for draw using camera
 
+Introducing the Air Doodle project – where your imagination takes flight with just the flick of a finger! Ever dreamed of creating art without lifting a brush? Well, get ready to turn that dream into reality as we dive into the exciting world of computer vision and machine learning.
+
+With the Air Doodle, you have the power to draw anything your heart desires using nothing but the motion of your hands. By tracking the landmarks on your hand, including the fingertips, we can transform your gestures into pure creativity.
+
+
+# Working
+
+
+#### 1. Hand Detection: Using OpenCV's computer vision techniques, our project will detect your hand from the background .
+
+
+#### 2. Landmark Detection: With the help of Mediapipe, or project will track the landmarks on your hand with remarkable precision. From knuckles to fingertips, every movement will be captured in real-time.
+
+
+#### 3.Motion Tracking: As you wave your hand through the air, our system will track its every move, translating your gestures into strokes on our virtual canvas.
+
+## Packages Required
+
+- opencv-python
+- mediapipe
+- numpy
+- python Interpretor 3.8
+
+### Clone the project using
+```
+git clone https://github.com/kush788/Air_Doodle.git
+cd Air_Doodle
+```
 
 ### create virtual environment and activate
 ```
-python -m venv <env-name>
-cd <env-name>/Scripts
-.\activate
+source <venv>/bin/activate   # On Linux/Mac
+<venv>\Scripts\activate      # On Windows
+cd ../..
+```
+- Replace <venv with the path to your virtual environment directory.
+
+
+Install packages using
+```
+pip install -r requirements.txt
 ```
 
-- rgtg
-- vfd
-- dv
-
-`dffgf`
-
-**dff**: hello
+Run the project using:
+```angular2html
+python "Air Doodle.py"
+```
 
